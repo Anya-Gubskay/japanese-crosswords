0333a4107bf0875e4b7b784bac1109c438a92400
Anna Gubskaya <anna.gubskaya@dif.tech>
2024-11-25 21:29:07 +0300

    refactoring code

--- src/app/utils/video-autoplay.ts
+++ src/app/utils/video-autoplay.ts
@@ -1,11 +1,12 @@
 export function setVideoAutoplay() {
-  const videos: NodeListOf<HTMLVideoElement> = document.querySelectorAll('video');
+  const videos: NodeListOf<HTMLVideoElement> =
+    document.querySelectorAll('video');
   videos.forEach((video) => {
     video.muted = true;
     video.autoplay = true;
     video.loop = true;
-    video.play().catch(error => {
-      console.log("Автоматическое воспроизведение не удалось:", error);
+    video.play().catch((error) => {
+      console.log('Автоматическое воспроизведение не удалось:', error);
     });
   });
 }
